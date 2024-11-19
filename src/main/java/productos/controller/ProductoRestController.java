2a177b2a74930f346de565e020f056d1e632dfff
andres_lopez <felifit27@gmail.com>
2024-11-18 20:11:50 -0500

    configurando el cors

--- src/main/java/productos/controller/ProductoRestController.java
+++ src/main/java/productos/controller/ProductoRestController.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@CrossOrigin(origins = "https://apiproductos-pbhp.onrender.com", maxAge = 3600)
+@CrossOrigin(origins = "https://127.0.0.1:5500", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class ProductoRestController {
