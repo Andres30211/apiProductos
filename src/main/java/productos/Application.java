c1b85a30128d6030ade4f5d22fa020a5cedc6e6e
andres_lopez <felifit27@gmail.com>
2024-11-20 19:32:42 -0500

    otra confing de cors global

--- src/main/java/productos/Application.java
+++ src/main/java/productos/Application.java
@@ -2,6 +2,9 @@ package productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +12,19 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todas las rutas
+                        .allowedOrigins("http://127.0.0.1:5500", "http://127.0.0.1:5501") // Dominios permitidos
+                        .allowedMethods("*") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Encabezados permitidos
+                        .allowCredentials(true); // Permitir envío de credenciales (cookies, etc.)
+            }
+        };
+    }
 
 }
